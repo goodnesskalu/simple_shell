b937c739fe4a264661c8493edc45facd83aa2d96
goodnesskalu <kalugoodnessi@gmail.com>
2022-11-16 14:18:11 +0000

    corrections

--- shell.c
+++ shell.c
@@ -1,89 +1,87 @@
-#include "shell.h"
+#include "holberton.h"
 
 /**
- * sig_handler - checks if Ctrl C is pressed
- * @sig_num: int
+ * execute - executes the command
+ * @cmd: command to run
+ * Return: 0 on success1 -1 if cmd is exit and 1 on any other error
  */
-void sig_handler(int sig_num)
+int execute(char **cmd)
 {
-	if (sig_num == SIGINT)
+
+	pid_t child_pid;
+	int status;
+
+	if (strncmp("exit", cmd[0], 4) == 0)
+		return (-1);
+
+	child_pid = fork();
+
+	if (child_pid == -1)
 	{
-		_puts("\n#cisfun$ ");
+		perror("Error");
+		return (1);
 	}
-}
-
-/**
-* _EOF - handles the End of File
-* @len: return value of getline function
-* @buff: buffer
- */
-void _EOF(int len, char *buff)
-{
-	(void)buff;
-	if (len == -1)
+	else if (child_pid == 0)
 	{
-		if (isatty(STDIN_FILENO))
+		if (execve(cmd[0], cmd, NULL) == -1)
 		{
-			_puts("\n");
-			free(buff);
-		}
-		exit(0);
-	}
-}
-/**
-  * _isatty - verif if terminal
-  */
-
-void _isatty(void)
-{
-	if (isatty(STDIN_FILENO))
-		_puts("#cisfun$ ");
-}
-/**
- * main - Shell
- * Return: 0 on success
- */
-
-int main(void)
-{
-	ssize_t len = 0;
-	char *buff = NULL, *value, *pathname, **arv;
-	size_t size = 0;
-	list_path *head = '\0';
-	void (*f)(char **);
-
-	signal(SIGINT, sig_handler);
-	while (len != EOF)
-	{
-		_isatty();
-		len = getline(&buff, &size, stdin);
-		_EOF(len, buff);
-		arv = splitstring(buff, " \n");
-		if (!arv || !arv[0])
-			execute(arv);
-		else
-		{
-			value = _getenv("PATH");
-			head = linkpath(value);
-			pathname = _which(arv[0], head);
-			f = checkbuild(arv);
-			if (f)
-			{
-				free(buff);
-				f(arv);
-			}
-			else if (!pathname)
-				execute(arv);
-			else if (pathname)
-			{
-				free(arv[0]);
-				arv[0] = pathname;
-				execute(arv);
-			}
+			perror("Error");
+			exit(-1);
 		}
 	}
-	free_list(head);
-	freearv(arv);
-	free(buff);
+	else
+		wait(&status);
+
+	return (0);
+}
+
+
+/**
+ * main - main simple shell
+ * @argc: number of arguments
+ * @argv: list of command line arguments
+ * Return: Always 0, -1 on error.
+ */
+
+int main(int argc, char **argv)
+{
+
+	int response;
+	char **tokens;
+	size_t bufsize = BUFSIZ;
+	int isPipe = 0;
+	char *buffer;
+
+	if (argc >= 2)
+	{
+		/*TODO: Handle cases where there is no argument, only the command*/
+		if (execve(argv[1], argv, NULL) == -1)
+		{
+			perror("Error");
+			exit(-1);
+		}
+		return (0);
+	}
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+
+	do {
+		if (isatty(fileno(stdin)))
+		{
+			isPipe = 1;
+			_puts("cisfun#: ");
+		}
+
+		getline(&buffer, &bufsize, stdin);
+		buffer[_strlen(buffer) - 1] = '\0';
+		tokens = stringToTokens(buffer);
+		response = execute(tokens);
+	} while (isPipe && response != -1);
+
 	return (0);
 }
